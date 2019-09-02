badefece63ba13b13411cfde929efaf197730c9e
Vladyslav Pekker <agilesteel@gmail.com>
2019-09-02 16:43:36 +0200

    Update install-intellij-idea-community.sh

--- install-intellij-idea-community.sh
+++ install-intellij-idea-community.sh
@@ -5,6 +5,10 @@ sudo snap install --classic intellij-idea-community
 
 echo
 
+echo '"intellij-idea-community" is now on the path'
+
+echo
+
 # update intellij-idea-community
 sudo snap refresh intellij-idea-community
 
